e28c5c4811002c27ce9bd142547e8f23ff532365
Cytonn <cytonn@LEGAL-PWANJAMA.local>
2019-03-20 16:46:49 +0300

    add user app

--- noticeboard/board/models.py
+++ noticeboard/board/models.py
@@ -4,4 +4,5 @@ from django.db import models
 # Create your models here.
 
 class Lead(models.Model):
-    
+    name = models.CharField(max_length=100)
+    department = models.CharField(max_length=200)
