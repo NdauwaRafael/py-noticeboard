f13cd3c05f2c9808076f6037a2c43276bc442e37
Raphael Ndauwa <raphaelndauwa@gmail.com>
2019-03-20 18:09:52 +0300

    Add UserViewset class for user api

--- noticeboard/user/api.py
+++ noticeboard/user/api.py
@@ -1,3 +1,12 @@
 from user.models import User
-from rest_framework import viewsets
+from rest_framework import viewsets, permissions
 from .serializers import UserSerializer
+
+
+# User View Set
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.ojects.all()
+    permission_classes = [
+        permissions.allowAny()
+    ]
+    serializer_class = UserSerializer
