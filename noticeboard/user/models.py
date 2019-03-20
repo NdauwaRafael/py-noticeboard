c1e5d17f82834d50219b9401ac2d42c0d061010d
Cytonn <cytonn@LEGAL-PWANJAMA.local>
2019-03-20 17:42:51 +0300

    create user migration file

--- noticeboard/user/models.py
+++ noticeboard/user/models.py
@@ -7,3 +7,5 @@ class User(models.Model):
     firstname = models.CharField(max_length=100)
     lastname = models.CharField(max_length=100)
     email = models.EmailField(max_length=100, unique=True)
+    password = models.CharField(max_length=200)
+    created_at = models.DateTimeField(auto_now_add=True)
