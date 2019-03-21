b35f85a61d5d0a3ba91bbcb87897f027aa8eca24
Raphael Ndauwa <raphaelndauwa@gmail.com>
2019-03-21 15:09:32 +0300

    add a template file

--- noticeboard/frontend/src/components/App.js
+++ noticeboard/frontend/src/components/App.js
@@ -1,7 +1,13 @@
 import React, {Component} from 'react';
+import ReactDOM from 'react-dom';
 
 class App extends Component {
+    render() {
+        return (
+            <h1>Hello tomorrow</h1>
+        );
 
+    }
 }
 
-export default App;
+ReactDOM.render(<App/>, document.getElementById('app'));
