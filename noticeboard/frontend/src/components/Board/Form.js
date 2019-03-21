8c9d06ef880bcb3dffd3ee0719be1792897675dd
Raphael Ndauwa <raphaelndauwa@gmail.com>
2019-03-21 23:43:38 +0300

    add basic boiler plates for newly created files

---
+++ noticeboard/frontend/src/components/Board/Form.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+class PostForm extends Component{
+    render() {
+        return (
+            <h1>Bord Post Form</h1>
+        )
+    }
+}
+
+export default PostForm;
---
+++ noticeboard/frontend/src/components/Board/Posts.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+class Posts extends Component{
+    render() {
+        return (
+            <h1>Bord Posts</h1>
+        )
+    }
+}
+
+export default Posts;
