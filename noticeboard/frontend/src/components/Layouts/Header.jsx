330db2c4083f92c15e4ccf0dba9eb5aedc471704
Raphael Ndauwa <raphaelndauwa@gmail.com>
2019-03-21 22:56:46 +0300

    add boiler plate for topbar

---
+++ noticeboard/frontend/src/components/Layouts/Header.jsx
@@ -0,0 +1,85 @@
+import React, {Component} from 'react';
+
+class Header extends Component{
+    render() {
+        return(
+            <div className="navbar navbar-expand-lg fixed-top navbar-light bg-light">
+                <div className="container">
+                    <a href="#" className="navbar-brand">Bootswatch</a>
+                    <button className="navbar-toggler" type="button" data-toggle="collapse"
+                            data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false"
+                            aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"> </span>
+                    </button>
+                    <div className="collapse navbar-collapse" id="navbarResponsive">
+                        <ul className="navbar-nav">
+                            <li className="nav-item dropdown">
+                                <a className="nav-link dropdown-toggle" data-toggle="dropdown" href="#"
+                                   id="themes">Themes <span className="caret"> </span></a>
+                                <div className="dropdown-menu" aria-labelledby="themes">
+                                    <a className="dropdown-item" href="#">Default</a>
+                                    <div className="dropdown-divider"> </div>
+                                    <a className="dropdown-item" href="#">Cerulean</a>
+                                    <a className="dropdown-item" href="#">Cosmo</a>
+                                    <a className="dropdown-item" href="#">Cyborg</a>
+                                    <a className="dropdown-item" href="#">Darkly</a>
+                                    <a className="dropdown-item" href="#">Flatly</a>
+                                    <a className="dropdown-item" href="#">Journal</a>
+                                    <a className="dropdown-item" href="#">Litera</a>
+                                    <a className="dropdown-item" href="#">Lumen</a>
+                                    <a className="dropdown-item" href="#">Lux</a>
+                                    <a className="dropdown-item" href="#">Materia</a>
+                                    <a className="dropdown-item" href="#">Minty</a>
+                                    <a className="dropdown-item" href="#">Pulse</a>
+                                    <a className="dropdown-item" href="#">Sandstone</a>
+                                    <a className="dropdown-item" href="#">Simplex</a>
+                                    <a className="dropdown-item" href="#">Sketchy</a>
+                                    <a className="dropdown-item" href="#">Slate</a>
+                                    <a className="dropdown-item" href="#">Solar</a>
+                                    <a className="dropdown-item" href="#">Spacelab</a>
+                                    <a className="dropdown-item" href="#">Superhero</a>
+                                    <a className="dropdown-item" href="#">United</a>
+                                    <a className="dropdown-item" href="#">Yeti</a>
+                                </div>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="#">Help</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="#">Blog</a>
+                            </li>
+                            <li className="nav-item dropdown">
+                                <a className="nav-link dropdown-toggle" data-toggle="dropdown" href="#"
+                                   id="download">Sketchy <span className="caret"> </span></a>
+                                <div className="dropdown-menu" aria-labelledby="download">
+                                    <a className="dropdown-item" target="_blank"
+                                       href="#">Open in JSFiddle</a>
+                                    <div className="dropdown-divider"> </div>
+                                    <a className="dropdown-item" href="#"
+                                       download>bootstrap.min.css</a>
+                                    <a className="dropdown-item" href="#"
+                                       download>bootstrap.css</a>
+                                    <div className="dropdown-divider"> </div>
+                                    <a className="dropdown-item" href="#"
+                                       download>_variables.scss</a>
+                                    <a className="dropdown-item" href="#"
+                                       download>_bootswatch.scss</a>
+                                </div>
+                            </li>
+                        </ul>
+
+                        <ul className="nav navbar-nav ml-auto">
+                            <li className="nav-item">
+                                <a className="nav-link" href="#"
+                                   target="_blank">WrapBootstrap</a>
+                            </li>
+                        </ul>
+
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Header;
