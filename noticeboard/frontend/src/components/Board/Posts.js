d7b6af91ef48ea6ea523c2ee21626380f1e8ba2c
Raphael Ndauwa <raphaelndauwa@gmail.com>
2019-03-22 14:57:08 +0300

    bind props and actions from redux to the component

--- noticeboard/frontend/src/components/Board/Posts.js
+++ noticeboard/frontend/src/components/Board/Posts.js
@@ -1,8 +1,12 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { getPosts } from '../../Redux/actions/posts';
+import { bindActionCreators } from 'redux';
+import { getAllPosts } from '../../Redux/actions/posts/index';
+
 class Posts extends Component {
     render() {
-        const { posts } = this.props;
+        const { posts } = this.props.posts;
         return (
             <h1>Posts </h1>
 
@@ -13,6 +17,12 @@ class Posts extends Component {
 const mapStateToProps = ({ posts }) => {
     return {
         posts
+    }
+};
+
+const mapDispatchToProps = (dispatch) => {
+    return {
+        getAllPosts: bindActionCreators(getPosts)
     }
 }
 export default connect(mapStateToProps)(Posts);
