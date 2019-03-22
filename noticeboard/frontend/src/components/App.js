97baa3119ce98dd34830ae2dd828b0de11c87836
Raphael Ndauwa <raphaelndauwa@gmail.com>
2019-03-22 12:28:05 +0300

    Connect redux store to app component
    Signed-off-by: Raphael Ndauwa <raphaelndauwa@gmail.com>

--- noticeboard/frontend/src/components/App.js
+++ noticeboard/frontend/src/components/App.js
@@ -1,6 +1,8 @@
 import React, {Component, Fragment} from 'react';
 import Header from './Layouts/Header';
-import ReactDOM from 'react-dom';
+import {render} from 'react-dom';
+import {Provider} from 'react-redux';
+import store from '../Redux';
 
 class App extends Component {
     render() {
@@ -13,4 +15,8 @@ class App extends Component {
     }
 }
 
-ReactDOM.render(<App />, document.getElementById('app'));
+render(
+    <Provider>
+        <App />
+    </Provider>, 
+    document.getElementById('app'));
