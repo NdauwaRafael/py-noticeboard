f373587e152e5317c69106c83d07ffbee92a2fe8
Raphael Ndauwa <raphaelndauwa@gmail.com>
2019-03-22 18:22:39 +0300

    validate add post method first before submitting

--- noticeboard/frontend/src/components/Board/Post/Form.js
+++ noticeboard/frontend/src/components/Board/Post/Form.js
@@ -8,21 +8,62 @@ class PostForm extends Component {
 
         this.state = {
             title: '',
-            decscription: '',
+            description: '',
             errors: {
                 title: '',
-                decscription: ''
+                description: ''
             }
         }
+
+        this.handleChange = this.handleChange.bind(this);
+        this.onSave = this.onSave.bind(this);
     }
+
+    handleChange(event) {
+        let field = event.target.name;
+        let value = event.target.value;
+        return this.setState({ field: value });
+    };
+
+    postIsValid() {
+        let { title, description, errors } = this.state;
+        let isValid = true;
+
+        if (title.length <= 3) {
+            errors.title = 'Post title must be at least 3 characters';
+            isValid = false;
+        } else {
+            errors.title = ''
+        }
+
+        if (description.length <= 20) {
+            errors.description = 'Description is too short. Provide at least 20 characters';
+            isValid = false;
+        } else {
+            errors.description = ''
+        }
+
+
+        this.setState({ errors });
+
+        return isValid;
+    }
+
+    onSave(e) {
+        e.preventDefault();
+        if (!this.postIsValid()) {
+            return;
+        }
+    }
+
     render() {
-        const { title, decscription } = this.state
+        const { title, description } = this.state
         return (
             <Fragment>
-                <form>
+                <form onSubmit={this.onSave()}>
                     <TextInput name='title' label="Post Title" value={title} error={errors.title} onChange={this.handleInputChange()} />
 
-                    <Textarea name="description" label="Post Description" value={decscription} error={errors.decscription} onChange={this.handleInputChange()} />
+                    <Textarea name="description" label="Post Description" value={description} error={errors.description} onChange={this.handleInputChange()} />
                     <button type="submit" class="btn btn-primary">Submit</button>
                 </form>
 
