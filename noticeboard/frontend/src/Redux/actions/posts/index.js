d8c61bfcf2c6ba31c41acddc7bd113dd30b8ed05
Raphael Ndauwa <raphaelndauwa@gmail.com>
2019-03-22 13:33:05 +0300

    add getPosts actions method

--- noticeboard/frontend/src/Redux/actions/posts/index.js
+++ noticeboard/frontend/src/Redux/actions/posts/index.js
@@ -1,17 +1,32 @@
 import {
     GET_POSTS_SUCCESS,
+    GET_POSTS_FAILED,
     ADD_POST_SUCCESS,
     DELETE_POST_SUCCESS
 } from '../../constants/actionTypes';
 import { getPosts } from '../../constants/API/posts';
 
 //GET POSTs
+export const getPostsSuccess = (resp) => {
+    return {
+        type: GET_POSTS_SUCCESS,
+        posts: resp
+    }
+};
+
+export const getPostsFailed = (resp) => {
+    return {
+        action: GET_POSTS_FAILED,
+        error: resp
+    }
+}
+
 export const getAllPosts = () => dispatch => {
     getPosts()
         .then(resp => {
-
+            return dispatch(getPostsSuccess(resp));
         })
         .catch(error => {
-
+            return dispatch(getPostsFailed(error.toString()))
         })
 }
