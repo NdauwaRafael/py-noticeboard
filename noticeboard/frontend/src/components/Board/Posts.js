d08c51fe5eb65d0ac7e41caf9707d02e1e8d322e
Raphael Ndauwa <raphaelndauwa@gmail.com>
2019-03-22 15:04:52 +0300

    display posts in posts component

--- noticeboard/frontend/src/components/Board/Posts.js
+++ noticeboard/frontend/src/components/Board/Posts.js
@@ -5,11 +5,38 @@ import { bindActionCreators } from 'redux';
 import { getAllPosts } from '../../Redux/actions/posts/index';
 
 class Posts extends Component {
+    componentDidMount() {
+        this.props.getAllPosts();
+    }
     render() {
         const { posts } = this.props.posts;
         return (
             <h1>Posts </h1>
+            <table class="table">
+                <thead class="thead-dark">
+                    <tr>
+                        <th scope="col">#</th>
+                        <th scope="col">Title</th>
+                        <th scope="col">Description</th>
+                        <th scope="col">Action</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {
+                        posts.map(post => (
+                            <tr key={post.id}>
+                                <th scope="row">{post.id}</th>
+                                <td>{post.title}</td>
+                                <td>{post.descritpion}</td>
+                                <td>
+                                    <button type="button" class="btn btn-outline-success">View</button>
+                                </td>
+                            </tr>
+                        ))
+                    }
 
+                </tbody>
+            </table>
         )
     }
 }
