4b927c940aa65cf31a2969bca33042e15255b22c
Raphael Ndauwa <raphaelndauwa@gmail.com>
2019-03-22 14:00:45 +0300

    fix identation error, add migration files

--- noticeboard/noticeboard/settings.py
+++ noticeboard/noticeboard/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'frontend',
     'board',
+    'posts',
     'user',
     'roles',
     'permissions',
--- noticeboard/posts/serializers.py
+++ noticeboard/posts/serializers.py
@@ -1,6 +1,7 @@
- from rest_framework import serializers
- from .models import Post
+from rest_framework import serializers
+from .models import Post
 
- class PostSerializer(serializers.ModelSerializer)
-    model = Post
-    fields = '__all__'
+
+class PostSerializer(serializers.ModelSerializer):
+   model = Post
+   fields = '__all__'
