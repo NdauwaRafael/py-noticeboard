6d80591294bff07eed27850f9da42392b8be5006
Raphael Ndauwa <raphaelndauwa@gmail.com>
2019-03-23 17:17:26 +0300

    add auth functional components

---
+++ noticeboard/frontend/src/components/auth/partials/LoginForm.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const LoginForm = props => {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+LoginForm.propTypes = {
+
+}
+
+export default LoginForm
---
+++ noticeboard/frontend/src/components/auth/partials/RegisterForm.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+function RegisterForm(props) {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+RegisterForm.propTypes = {
+
+}
+
+export default RegisterForm
+
