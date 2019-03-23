94c5745ee02101c317960ee6b7f06c6257b5940a
Raphael Ndauwa <raphaelndauwa@gmail.com>
2019-03-23 17:56:28 +0300

    use props passed in loginfor component

--- noticeboard/frontend/src/components/auth/partials/LoginForm.js
+++ noticeboard/frontend/src/components/auth/partials/LoginForm.js
@@ -2,11 +2,25 @@ import React, { Fragment } from 'react'
 import PropTypes from 'prop-types'
 import TextInput from '../../common/form/Input';
 
-const LoginForm = props => {
+const LoginForm = ({ username, password, onChange, onSave, errors }) => {
     return (
         <Fragment>
-            <form>
+            <form onSubmit={onSave}>
+                <TextInput
+                    name='username'
+                    label="Username"
+                    value={username}
+                    error={errors.username}
+                    onChange={onChange} />
 
+                <TextInput
+                    name='password'
+                    label="password"
+                    value={password}
+                    error={errors.password}
+                    onChange={onChange} />
+
+                <button type="submit" className="btn btn-primary">Submit</button>
             </form>
         </Fragment>
     )
