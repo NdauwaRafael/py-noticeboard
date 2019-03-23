c160db7e5d3fe903a1cee454bbe817a52ee46d6f
Raphael Ndauwa <raphaelndauwa@gmail.com>
2019-03-23 11:28:33 +0300

    ädd owner and owner migration file

--- noticeboard/posts/models.py
+++ noticeboard/posts/models.py
@@ -1,8 +1,9 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Post(models.Model):
     title = models.CharField(max_length=100, unique=True)
     description = models.CharField(max_length = 500)
+    owner = models.ForeignKey(User, related_name="posts", on_delete=models.CASCADE, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
