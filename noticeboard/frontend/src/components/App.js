b95e58c1ac74e3bf557d630e97fb1945fd76e1ef
Raphael Ndauwa <raphaelndauwa@gmail.com>
2019-03-23 17:11:29 +0300

    add route file and include it in root component

--- noticeboard/frontend/src/components/App.js
+++ noticeboard/frontend/src/components/App.js
@@ -1,10 +1,11 @@
 import React, { Component, Fragment } from 'react';
 import Header from './Layouts/Header';
 import { render } from 'react-dom';
+import { HashRouter as Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from '../Redux';
-import Dashboard from './Board/Dashboard';
 import Alerts from './Layouts/Alerts';
+import AppRoutes from '../router'
 //ALERTS
 import { transitions, Provider as AlertProvider } from 'react-alert';
 import AlertTemplate from 'react-alert-template-basic';
@@ -18,11 +19,15 @@ const alertOptions = {
 class App extends Component {
     render() {
         return (
-            <Fragment>
-                <Header />
-                <Alerts />
-                <Dashboard />
-            </Fragment>
+            <Router>
+                <Fragment>
+                    <Header />
+                    <Alerts />
+                    {
+                        AppRoutes
+                    }
+                </Fragment>
+            </Router>
         );
 
     }
--- noticeboard/frontend/src/router/index.js
+++ noticeboard/frontend/src/router/index.js
@@ -1,2 +1,9 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
+import Dashboard from '../components/Board/Dashboard';
+
+export default (
+    <Switch>
+        <Route path="/" exact component={Dashboard} />
+    </Switch>
+)
